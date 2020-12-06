16cd7ea0c1b8375ad7ce00d2c60f9399163fdded
abel <abel09@qq.com>
2020-12-06 13:15:31 +0800

    i18n

---
+++ src/main/resources/static/layui-ext/i18n/i18n.js
@@ -0,0 +1,18 @@
+layui.define(function (exports) {
+    "use strict";
+    var $ = layui.jquery;
+
+    $.i18n.properties({
+        name: 'i18n', // 资源文件名称
+        path: '../i18n/', //资源文件所在目录路径
+        mode: 'map', // 模式：变量或 Map
+        language: 'cn', // 对应的语言
+        cache: false,
+        encoding: 'UTF-8',
+        callback: function () {
+            //$('#layui-logo').html($.i18n.prop('a'));
+        }
+    });
+
+    exports('i18n', {});
+});
