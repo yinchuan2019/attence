7b65749018130b11da20063b5cbab0bed626f53c
za-yinchuan <abel09@qq.com>
2021-02-14 20:40:44 +0800

    four

---
+++ src/main/java/com/my/attence/controller/EmailController.java
@@ -0,0 +1,99 @@
+package com.my.attence.controller;
+
+import com.my.attence.common.R;
+import com.my.attence.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+/**
+ * Created by abel on 2021/2/14
+ * TODO
+ */
+
+@RestController
+@RequestMapping("email")
+public class EmailController {
+    @Autowired
+    private MailService mailService;
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @PostMapping
+    public R index(){
+        try {
+            mailService.sendSimpleMail("abel09@qq.com","这是一封普通的邮件","这是一封普通的测试邮件");
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return new R(-1,"邮件发送失败!!");
+        }
+        return new R();
+    }
+
+    @PostMapping("/htmlEmail")
+    public R htmlEmail(){
+        try {
+            mailService.sendHtmlMail("11111@qq.com","这是一HTML的邮件","<body style=\"text-align: center;margin-left: auto;margin-right: auto;\">\n"
+                    + "	<div id=\"welcome\" style=\"text-align: center;position: absolute;\" >\n"
+                    +"		<h3>欢迎使用IJPay -By Javen</h3>\n"
+                    +"		<span>https://github.com/Javen205/IJPay</span>"
+                    + "		<div\n"
+                    + "			style=\"text-align: center; padding: 10px\"><a style=\"text-decoration: none;\" href=\"https://github.com/Javen205/IJPay\" target=\"_bank\" ><strong>IJPay 让支付触手可及,欢迎Start支持项目发展:)</strong></a></div>\n"
+                    + "		<div\n" + "			style=\"text-align: center; padding: 4px\">如果对你有帮助,请任意打赏</div>\n"
+                    + "		<img width=\"180px\" height=\"180px\"\n"
+                    + "			src=\"https://oscimg.oschina.net/oscnet/8e86fed2ee9571eb133096d5dc1b3cb2fc1.jpg\">\n"
+                    + "	</div>\n" + "</body>");
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return new R(-1,"邮件发送失败!!");
+        }
+        return new R();
+    }
+
+    @PostMapping("/attachmentsMail")
+    public R attachmentsMail(){
+        try {
+            String filePath = "C:\\Users\\Administrator\\Pictures\\Feedback\\1.jpg";
+            mailService.sendAttachmentsMail("11111@qq.com", "这是一封带附件的邮件", "邮件中有附件，请注意查收！", filePath);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return new R(-1,"邮件发送失败!!");
+        }
+        return new R();
+    }
+
+    @PostMapping("/resourceMail")
+    public R resourceMail(){
+        try {
+            String rscId = "IJPay";
+            String content = "<html><body>这是有图片的邮件<br/><img src=\'cid:" + rscId + "\' ></body></html>";
+            String imgPath = "/Users/Javen/Desktop/IJPay.png";
+            mailService.sendResourceMail("11111@163.com@163.com", "这邮件中含有图片", content, imgPath, rscId);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return new R(-1,"邮件发送失败!!");
+        }
+        return new R();
+    }
+
+    @PostMapping("/templateMail")
+    public R templateMail(){
+        try {
+            Context context = new Context();
+            context.setVariable("project", "IJPay");
+            context.setVariable("author", "Javen");
+            context.setVariable("url", "https://github.com/Javen205/IJPay");
+            String emailContent = templateEngine.process("emailTemp", context);
+
+            mailService.sendHtmlMail("11111@qq.com", "这是模板邮件", emailContent);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return new R(-1,"邮件发送失败!!");
+        }
+        return new R();
+    }
+}
